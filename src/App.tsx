013233d0e5f5b761ca3d01912a50bd4ae4a30a68
astralaria <pawut.j@gmail.com>
2023-03-06 19:28:47 +0700

    Add Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useUserContext } from "./context/User/UserContext";
 
 function App() {
+  const {
+    state: { user },
+  } = useUserContext();
+
+  const { dispath } = useUserContext();
+
+  const login = () => {
+    dispath({
+      type: "LOG_IN",
+      payload: { user: "test" },
+    });
+  };
+
+  const checkLogin = () => {
+    console.log(user, "login state");
+  };
+
   return (
     <div className="App">
+      <button type="button" onClick={login}>
+        Login
+      </button>
+      <button onClick={checkLogin}>Check Login</button>
+      {/* <h1>{state.user}</h1> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { UserContextProvider } from "./context/User/UserContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <UserContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </UserContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
